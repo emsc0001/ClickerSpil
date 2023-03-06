1b033f4e390d528516b363c2ccff96357305ee43
emsc0001 <124031667+emsc0001@users.noreply.github.com>
2023-03-06 11:12:16 +0100

    Animation reset og mere lyd

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict";
 window.addEventListener("load", ready);
 
-
+document.body.style.overflow = "hidden";
 
 let points = 0;
 let lives = 0;
@@ -12,16 +12,12 @@ function ready() {
 }
 
 function startGame() {
- 
   points = 0;
   lives = 3;
-
 
   document.querySelector("#start").classList.add("hidden");
 
   startAllAnimations();
-
-
 
   document.querySelector("#zombie1").classList.add("zombie1");
   document.querySelector("#zombie1").addEventListener("click", clickZombie1);
@@ -53,6 +49,8 @@ function startGame() {
   document.querySelector("#zombie1").addEventListener("animationiteration", zombieRestart);
   document.querySelector("#zombie2").addEventListener("animationiteration", zombieRestart);
   document.querySelector("#zombie3").addEventListener("animationiteration", zombieRestart);
+
+  document.querySelector("#soundBG").play();
 }
 
 function startAllAnimations() {
@@ -63,13 +61,13 @@ function startAllAnimations() {
   document.querySelector("#zombie5").classList.add("zombie5");
 }
 
-
-
 function clickZombie1() {
   console.log("Click zombie");
   document.querySelector("#zombie1").removeEventListener("click", clickZombie1);
   document.querySelector("#zombie1").classList.add("paused");
   document.querySelector("#zombie1_sprite").classList.add("zoom_out");
+  document.querySelector("#zombie1").addEventListener("animationend", zombieGone);
+  document.querySelector("#zombie1").addEventListener("click", clickZombie1);
   document.querySelector("#soundMonster").currentTime = 0;
   document.querySelector("#soundMonster").play();
   incrementPoints();
@@ -80,6 +78,8 @@ function clickZombie2() {
   document.querySelector("#zombie2").removeEventListener("click", clickZombie2);
   document.querySelector("#zombie2").classList.add("paused");
   document.querySelector("#zombie2_sprite").classList.add("zoom_out");
+  document.querySelector("#zombie2").addEventListener("animationend", zombieGone);
+  document.querySelector("#zombie2").addEventListener("click", clickZombie2);
   document.querySelector("#soundMonster").currentTime = 0;
   document.querySelector("#soundMonster").play();
   incrementPoints();
@@ -90,6 +90,8 @@ function clickZombie3() {
   document.querySelector("#zombie3").removeEventListener("click", clickZombie3);
   document.querySelector("#zombie3").classList.add("paused");
   document.querySelector("#zombie3_sprite").classList.add("zoom_out");
+  document.querySelector("#zombie3").addEventListener("animationend", zombieGone);
+  document.querySelector("#zombie3").addEventListener("click", clickZombie3);
   document.querySelector("#soundMonster").currentTime = 0;
   document.querySelector("#soundMonster").play();
   incrementPoints();
@@ -100,6 +102,8 @@ function clickZombie4() {
   document.querySelector("#zombie4").removeEventListener("click", clickZombie4);
   document.querySelector("#zombie4").classList.add("paused");
   document.querySelector("#zombie4_sprite").classList.add("zoom_out");
+  document.querySelector("#zombie4").addEventListener("animationend", zombieGone);
+  document.querySelector("#zombie4").addEventListener("click", clickZombie4);
   document.querySelector("#soundMonster").currentTime = 0;
   document.querySelector("#soundMonster").play();
   incrementPoints();
@@ -110,6 +114,8 @@ function clickZombie5() {
   document.querySelector("#zombie5").removeEventListener("click", clickZombie5);
   document.querySelector("#zombie5").classList.add("paused");
   document.querySelector("#zombie5_sprite").classList.add("zoom_out");
+  document.querySelector("#zombie5").addEventListener("animationend", zombieGone);
+  document.querySelector("#zombie5").addEventListener("click", clickZombie5);
   document.querySelector("#soundMonster").currentTime = 0;
   document.querySelector("#soundMonster").play();
   incrementPoints();
@@ -120,6 +126,9 @@ function clickbrain1() {
   document.querySelector("#brain1").removeEventListener("click", brain1);
   document.querySelector("#brain1").classList.add("paused");
   document.querySelector("#brain1_sprite").classList.add("zoom_out");
+  document.querySelector("#brain1").addEventListener("click", clickbrain1);
+  document.querySelector("#soundBrain").currentTime = 0;
+  document.querySelector("#soundBrain").play();
   decrementLives();
 }
 
@@ -128,6 +137,10 @@ function clickbrain2() {
   document.querySelector("#brain2").removeEventListener("click", brain2);
   document.querySelector("#brain2").classList.add("paused");
   document.querySelector("#brain2_sprite").classList.add("zoom_out");
+  document.querySelector("#brain2").addEventListener("animationend", zombieGone);
+  document.querySelector("#brain2").addEventListener("click", clickbrain2);
+  document.querySelector("#soundBrain").currentTime = 0;
+  document.querySelector("#soundBrain").play();
   decrementLives();
 }
 
@@ -142,45 +155,38 @@ function clickBird() {
 }
 
 function zombieGone() {
-  
-  const zombie = this; 
- 
+  const zombie = this;
+
   zombie.removeEventListener("animationend", zombieGone);
 
-
   zombie.querySelector("img").classList.remove("zoom_out");
-
 
   zombie.classList.remove("paused");
 
   zombieRestart.call(this);
 
-  zombie.addEventListener("click", clickZombie);
+  zombie.addEventListener("click", clickZombie1);
 }
 
 function zombieRestart() {
- 
   const zombie = this;
-
 
   zombie.classList.remove("zombie1");
   zombie.offsetWidth;
   zombie.classList.add("zombie1");
 
-  
   zombie.classList.remove("position1", "position2", "position3", "position4", "position5");
 
- 
   const p = Math.ceil(Math.random() * 5);
   zombie.classList.add(`position${p}`);
 }
 
 function incrementPoints() {
-  console.log("Giv point");
   points++;
+  console.log("Giv point");
   displayPoints();
 
-  if (points >= 4) {
+  if (points >= 15) {
     levelcomplete();
   }
 }
@@ -188,6 +194,11 @@ function incrementPoints() {
 function displayPoints() {
   console.log("vis point");
   document.querySelector("#coin_count").textContent = points;
+}
+
+function levelcomplete() {
+  document.querySelector("#level_complete").classList.remove("hidden");
+  stop();
 }
 
 function decrementLives() {
@@ -204,20 +215,10 @@ function decrementLives() {
     document.querySelector("#heart" + (lives + 1)).classList.remove("active_heart");
   }
 
-  function showIncrementedLives() {
-    document.querySelector("#heart" + lives).classList.add("broken_heart");
-    document.querySelector("#heart" + lives).classList.remove("active_heart");
-  }
-
   function gameOver() {
     console.log("Game Over");
     document.querySelector("#game_over").classList.remove("hidden");
     stopGame();
-  }
-
-  function level_complete() {
-    document.querySelector("#level_complete").classList.remove("hidden");
-  
   }
 
   //TIMER
@@ -240,6 +241,6 @@ const timer = setInterval(() => {
 
   if (countdown === 0) {
     clearInterval(timer);
-    levelcomplete();
+    gameOver();
   }
 }, 1000);
